2a6f1dd257c0dcfd6b2d65dda92343cb89e796c1
FacundoStancanelliUTN <fstancanelli@frba.utn.edu.ar>
2024-04-15 13:37:01 +0000

    adicion de funciones para liberar memoria

---
+++ kernel/src/includes/utils/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <includes/config/config.h>
+#include <stdlib.h>
+
+void terminarPrograma(tKernelConfig* kernelConfig, t_log* kernelLogger);
+
+#endif /* UTILS_H_ */
