571ba0317cb663b7fe9347a3a99cc98ee4fdcabc
FacundoStancanelliUTN <fstancanelli@frba.utn.edu.ar>
2024-04-15 01:57:40 +0000

    modificacion codigos

---
+++ utils/src/shared/init.h
@@ -0,0 +1,10 @@
+#ifndef INIT_H_
+#define INIT_H_
+
+#include<commons/config.h>
+#include<commons/log.h>
+
+t_config* leerConfig(char* path, t_log* logger);
+t_log* iniciarLogger(char* loggerFilePath, char* moduleName, bool isActiveConsolem, t_log_level logLevel);
+
+#endif /* INIT_H_ */
---
+++ utils/src/shared/structs.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTS_H_
+#define STRUCTS_H_
+
+
+//Algoritmos de planificacion:
+
+typedef enum {
+     VRR,
+     FIFO,
+     UNKNOWN
+} tAlgoritmosPlanificacion;
+
+#endif /* STRUCTS_H_ */
