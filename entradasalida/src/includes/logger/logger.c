75d1b0698d25c2a4b2b678113280b863dbbc5040
jkarabin <jkarabin@frba.utn.edu.ar>
2024-04-21 15:21:27 +0000

    Borro archivos en entradasalida

--- entradasalida/src/includes/logger/logger.c
+++
@@ -1,7 +0,0 @@
-#include <includes/logger/logger.h>
-
-t_log* iniciarInterfazLogger()
-{
-	t_log* nuevo_logger = iniciarLogger(interfazLoggerFilePath, interfazModuleName, true, LOG_LEVEL_INFO);
-	return nuevo_logger;
-} 
