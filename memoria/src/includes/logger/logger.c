6185864c0e40f6b4086bc0b1611ee2cfdc06e315
Santino <santinorascovic@gmail.com>
2024-04-21 15:16:52 +0000

    conexion memoria

--- memoria/src/includes/logger/logger.c
+++
@@ -1,6 +0,0 @@
-#include <includes/logger/logger.h>
-
-t_log* iniciarMemoriaLogger(){
-	t_log* nuevo_logger = iniciarLogger(memoriaLoggerFilePath, memoriaModuleName, true, LOG_LEVEL_INFO);
-	return nuevo_logger;
-}
