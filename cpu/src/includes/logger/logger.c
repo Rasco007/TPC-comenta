74bb9abdad4e9925ebdc18644064e9ea7c30f130
jkarabin <jkarabin@frba.utn.edu.ar>
2024-04-21 15:19:58 +0000

    Cambios en CPU

--- cpu/src/includes/logger/logger.c
+++
@@ -1,6 +0,0 @@
-#include <includes/logger/logger.h>
-
-t_log* iniciarCPULogger(){
-	t_log* nuevo_logger = log_create(CPULoggerFilePath, CPUModuleName, true, LOG_LEVEL_INFO);
-	return nuevo_logger;
-}
