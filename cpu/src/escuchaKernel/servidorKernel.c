4158b5268bae975e1dff0599d90e8ac3ee497428
Uberg001 <uriel.eitan.aisenberg@gmail.com>
2024-04-25 20:26:45 +0000

    Punto de partida para checkpoint 2 (CPU)

--- cpu/src/escuchaKernel/servidorKernel.c
+++ cpu/src/escuchaKernel/servidorKernel.c
@@ -21,6 +21,29 @@ void escucharAlKernel() {
 
 
 int ejecutarServidorCPU(){
-	return 0;
+    instruccionActual = -1;
+		int codOP = recibirOperacion(socketCliente);
+		switch (codOP) {
+			case -1:
+				log_info(logger, "El Kernel se desconecto.");
+				if (contextoEjecucion != NULL)
+					destroyContexto ();
+				return EXIT_FAILURE;
+			case CONTEXTOEJECUCION:
+				if (contextoEjecucion != NULL) 
+					list_clean_and_destroy_elements (contextoEjecucion->instrucciones, free),
+					list_clean_and_destroy_elements (contextoEjecucion->tablaDeSegmentos, free);
+				recibirContextoActualizado(socketCliente);
+    			rafagaCPU = temporal_create(); 
+                while(contextoEjecucion->programCounter != (int) contextoEjecucion->instruccionesLength 
+					  && (noEsBloqueante(instruccionActual))) {
+                    cicloDeInstruccion();
+                }	
+				temporal_destroy (rafagaCPU);
+				break;
+			default:
+				log_warning(loggerError,"Operacion desconocida. No quieras meter la pata");
+				break;
+		}
 }
 
