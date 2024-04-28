adfb002ef5ab9105e39ad7905f23ccd542d4d948
Uberg001 <uriel.eitan.aisenberg@gmail.com>
2024-04-28 19:28:11 +0000

    Cambios en utils

--- utils/src/global.h
+++ utils/src/global.h
@@ -1,30 +1,19 @@
 #ifndef GLOBAL_H_
 #define GLOBAL_H_
 
-    // Librerias de UNIX
     #include <signal.h>
-    // Librerias de so-commons
+    
     #include <commons/log.h>
     #include <commons/config.h>
 
-    /**
-     * @enum t_comando
-     * @brief Lista de comandos enumerados.
-     */
-
     #define NUM_COMANDOS 16
     typedef enum {
-        SET, MOV_IN, MOV_OUT, IO,
-        F_OPEN, F_CLOSE, F_SEEK, F_READ, F_WRITE, F_TRUNCATE, F_CREATE,
-        WAIT, SIGNAL,
-        CREATE_SEGMENT, DELETE_SEGMENT,
-        YIELD, EXIT
+        SET, MOV_IN, MOV_OUT, SUM, SUB,
+        JNZ, RESIZE, COPY_STRING, WAIT, SIGNAL,
+        IO_GEN_SLEEP, IO_STDIN_READ, IO_STDOUT_WRITE, IO_FS_CREATE,
+        IO_FS_DELETE, IO_FS_TRUNCATE, IO_FS_WRITE, IO_FS_READ,
+        EXIT,
     } t_comando; 
-
-    /**
-     * @struct t_instruccion
-     * @brief Estructura que sostiene las instrucciones para enviar del Kernel a la CPU (O para que la CPU Parsee la informacion recibida.)
-     */
     typedef struct {
         t_comando comando; 
         char* parametros[3]; 
