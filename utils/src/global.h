22e41ba0cc5bf8b1789c7193a84059071d0d76b5
Uberg001 <uriel.eitan.aisenberg@gmail.com>
2024-06-12 02:18:32 +0000

    Planificacion. Volvio el problema de pcbsREADYaux

--- utils/src/global.h
+++ utils/src/global.h
@@ -19,7 +19,7 @@
         JNZ, RESIZE, COPY_STRING, WAIT, SIGNAL,
         IO_GEN_SLEEP, IO_STDIN_READ, IO_STDOUT_WRITE, IO_FS_CREATE,
         IO_FS_DELETE, IO_FS_TRUNCATE, IO_FS_WRITE, IO_FS_READ,
-        EXIT
+        EXIT, FIN_DE_QUANTUM,
     } t_comando; 
     typedef struct {
         t_comando comando; 
